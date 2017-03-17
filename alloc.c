d3c4195ebc4bb59973898f1ed16e8e2f6d1f4216
Adrian Perez de Castro <aperez@igalia.com>
2017-03-17 16:53:34 +0200

    Include <stdlib.h> instead of locally defining malloc/free

--- alloc.c
+++ alloc.c
@@ -1,7 +1,6 @@
 #include "alloc.h"
 #include "error.h"
-extern char *malloc();
-extern void free();
+#include <stdlib.h>
 
 #define ALIGNMENT 16 /* XXX: assuming that this alignment is enough */
 #define SPACE 4096 /* must be multiple of ALIGNMENT */
